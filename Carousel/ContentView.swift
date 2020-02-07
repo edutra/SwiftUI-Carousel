74d2ada4f04185af8d9e4e59b551080c3f1a632d
edutra <edutra@gmail.com>
2020-02-07 19:53:32 -0300

    first commit

---
+++ Carousel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Carousel
+//
+//  Created by Enrique Dutra on 07/02/20.
+//  Copyright © 2020 Dutra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
